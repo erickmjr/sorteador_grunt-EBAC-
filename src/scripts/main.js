ac990e4ade198d74fbfca1c5602e895f3b58be74
erickmjr <erickmellojr@gmail.com>
2025-01-10 20:50:16 -0300

    fix eventoDefault

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    document.getElementById('form-sorteador').addEventListener('submit', function(e) {
-        e.preventDefault();
+    document.getElementById('form-sorteador').addEventListener('submit', function(evento) {
+        evento.preventDefault();
         let numeroMaximo = document.getElementById('numero-maximo').value;
         numeroMaximo = parseInt(numeroMaximo);
 
